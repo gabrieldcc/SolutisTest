ba20e3ac66e19b16e8a1c708cc8275e256e70058
Gabriel Castro <t786832@f1rst.com.br>
2023-02-25 10:07:42 -0300

    Initial Commit

---
+++ TestSolutis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSolutis
+//
+//  Created by Gabriel de Castro Chaves on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
